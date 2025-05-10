9b579c369da243c7f2d8f08665453f9b8875e6a9
Muntaz1r <muntazirrasool212112@gmail.com>
2025-05-10 17:08:00 +0500

    fixed conflicts

--- classes/game/Game.cpp
+++ classes/game/Game.cpp
@@ -161,7 +161,6 @@ void Game::runGame() {
                     cameraOffsetX = playerCenterX - halfScreen; // follow and center the player
                 }
                 level->render(window, cameraOffsetX);
-                level->exitCheck(cameraOffsetX);
                 level->drawUI(window, cameraOffsetX);
             }
         }
