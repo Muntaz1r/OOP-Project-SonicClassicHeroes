27cc9d87d87c4a29104e2684f1a03ae9c5f1e7a4
Ms-1132 <Muhammad.saad.4761@gmail.com>
2025-05-11 19:32:57 +0500

    idr

--- classes/factories/PlayerFactory.h
+++ classes/factories/PlayerFactory.h
@@ -13,96 +13,23 @@ using namespace std;
 class PlayerFactory {
 public:
     virtual Player* createPlayer(float acc_x, float acc_y, float friction, float gravity) const = 0;
-    virtual ~PlayerFactory(){};
+    virtual ~PlayerFactory();
 };
 
+// Sonic Factory
 class SonicFactory : public PlayerFactory {
 public:
-    
-    Player* createPlayer(float acc_x, float acc_y, float friction, float gravity) const override {
-        sf::Texture *texture = new Texture;
-        if (!texture->loadFromFile("Data/0right.png")) {
-            cout << "Failed to load player texture\n";
-        }
-        return new Sonic(
-            50.0f,   // px
-            200.0f,   // py
-            80,      // height
-            80,      // width
-            texture,  // Texture pointer
-            0.0f,    // velocity_x
-            0.0f,    // velocity_y
-            4.0f,    // terminal_velocity
-            4.5f,    // maxSpeed
-            true,    // onGround
-            false,   // invincible
-            true,    // movingRight
-            acc_x,    // acc_x
-            acc_y,     // acc_y
-            friction,    // friction
-            gravity,   //gravity
-            7,  //special abillity duration
-            false // leader
-        );
-    }
+    Player* createPlayer(float acc_x, float acc_y, float friction, float gravity) const override;
 };
+
+// Tails Factory
 class TailsFactory : public PlayerFactory {
     public:
-        Player* createPlayer(float acc_x, float acc_y, float friction, float gravity) const override {
-            sf::Texture *texture = new Texture;
-            if (!texture->loadFromFile("Data/0right.png")) {
-                cout << "Failed to load player texture\n";
-            }
-            return new Tails(
-                10.0f,   // px
-                200.0f,   // py
-                80,      // height
-                80,      // width
-                texture,  // Texture pointer
-                0.0f,    // velocity_x
-                0.0f,    // velocity_y
-                4.0f,    // terminal_velocity
-                4.5f,    // maxSpeed
-                true,    // onGround
-                false,   // invincible
-                true,    // movingRight
-                acc_x,    // acc_x
-                acc_y,     // acc_y
-                friction,    // friction
-                gravity,   //gravity
-                7, // specail abillity time
-                false //leader
-            );
-        }
+        Player* createPlayer(float acc_x, float acc_y, float friction, float gravity) const override;
 };
     
 // Knuckles Factory
 class KnucklesFactory : public PlayerFactory {
 public:
-    Player* createPlayer(float acc_x, float acc_y, float friction, float gravity) const override {
-        sf::Texture *texture = new Texture;
-        if (!texture->loadFromFile("Data/0right.png")) {
-            cout << "Failed to load player texture\n";
-        }
-        return new Knuckles(
-            20.0f,   // px
-            200.0f,   // py
-            80,      // height
-            80,      // width
-            texture,  // Texture pointer
-            0.0f,    // velocity_x
-            0.0f,    // velocity_y
-            4.0f,    // terminal_velocity
-            4.5f,    // maxSpeed
-            true,    // onGround
-            false,   // invincible
-            true,    // movingRight
-            acc_x,    // acc_x
-            acc_y,     // acc_y
-            friction,    // friction
-            gravity,   //gravity
-            7, // power up abillity time
-            false //leader
-        );
-    }
+    Player* createPlayer(float acc_x, float acc_y, float friction, float gravity) const;
 };
